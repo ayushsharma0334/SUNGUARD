983e3f59adc40fb25bc9d2c7fead4fea88e264bb
ayushsharma0334 <33574578+ayushsharma0334@users.noreply.github.com>
2025-05-09 15:27:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,15 +9,21 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'picsum.photos',
+      port: '',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'vsdstudio.in',
+      port: '',
+      pathname: '/wp-content/uploads/2025/05/sssssssssssss.png',
+    },
+  ],
+},
 };
 
 export default nextConfig;
